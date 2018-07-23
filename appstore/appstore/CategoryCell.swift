3ac83866b6b43cd339d94d341c5081fc4d50aeb9
carlogilmar <carlogilmar12@gmail.com>
2018-07-23 01:04:57 -0500

    Adding category cell class

---
+++ appstore/appstore/CategoryCell.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryCell.swift
+//  appstore
+//
+//  Created by Carlo Gilmar on 23/07/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        print("Category cell initialize...")
+        print(frame.debugDescription)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Init coder has not been implemented!!!!")
+    }
+    
+    let appsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .blue
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    func setupViews(){
+        backgroundColor = .red
+        addSubview(appsCollectionView)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+    }
+}
