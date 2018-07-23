76e10d053e079f175f167f14c66347aa1b834ab5
carlogilmar <carlogilmar12@gmail.com>
2018-07-23 00:44:13 -0500

    Display 3 cells in view

--- appstore/appstore/ViewController.swift
+++ appstore/appstore/ViewController.swift
@@ -8,12 +8,55 @@
 
 import UIKit
 
+/*
+ 
+ todos:
+ 
+ - Create the UICollectionViewController
+ - Initialize controller in the AppDelegate
+ - Make the numbreOfItemsInSection function returning 3
+ - Complete the cellForItem function
+ - Create a class CategoryCell which implement UICollectionViewCell and initialize
+ - Complete the cellForItem function returning cellForItem
+ - Register the category cell
+ 
+ */
+
 class FeaturedAppsController: UICollectionViewController {
 
+    private let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .red
+        collectionView?.backgroundColor = .white
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
     }
 
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
+        return cell
+    }
+    
+    // items in collection view
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
 }
 
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        print("Category cell initialize...")
+        print(frame.debugDescription)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Init coder has not been implemented!!!!")
+    }
+    
+    func setupViews(){
+        backgroundColor = .red
+    }
+}
