1c6642c643fd39e2348b312bf1b01ca064e74b2c
carlogilmar <carlogilmar12@gmail.com>
2018-07-23 00:28:13 -0500

    Adding a UICollectionViewController

--- appstore/appstore/ViewController.swift
+++ appstore/appstore/ViewController.swift
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeaturedAppsController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView?.backgroundColor = .red
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
