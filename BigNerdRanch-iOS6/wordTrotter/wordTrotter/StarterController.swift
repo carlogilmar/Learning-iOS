548922a280e043d2afcbf03a2ea0c28a5d5f708d
carlogilmar <carlogilmar12@gmail.com>
2018-07-28 22:04:31 -0500

    Adding new UIViewController

---
+++ BigNerdRanch-iOS6/wordTrotter/wordTrotter/StarterController.swift
@@ -0,0 +1,18 @@
+//
+//  StarterController.swift
+//  wordTrotter
+//
+//  Created by Carlo Gilmar on 28/07/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import UIKit
+
+class StarterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+}
