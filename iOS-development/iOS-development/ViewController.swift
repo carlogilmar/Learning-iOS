1f1f282cc495cf56437a5d29df08335a56a88ca5
carlogilmar <carlogilmar12@gmail.com>
2018-08-13 00:20:31 -0500

    Adding other view controller in the storyboard

---
+++ iOS-development/iOS-development/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-development
+//
+//  Created by Carlo Gilmar on 13/08/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+}
+
