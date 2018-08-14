45cb0f1bf33e518e96d6b6baf5a915de2a46839b
carlogilmar <carlogilmar12@gmail.com>
2018-08-14 01:16:31 -0500

    Adding comments, in my Xcode with Xvim I couldn't put emojis and it's so sad 😭

--- itineraty/itineraty/Views/TripsViewController/TripsViewController.swift
+++ itineraty/itineraty/Views/TripsViewController/TripsViewController.swift
@@ -8,14 +8,45 @@
 
 import UIKit
 
-class TripsViewController: UIViewController {
+/*
+ 
+    You can implement protocols as UIViewController, UITableViewDataSource...
+    and you will have to implement their functions in your controller
+        THIS IS THE DELEGATE PATTERN
+ 
+ */
+class TripsViewController: UIViewController, UITableViewDataSource{
     
+    // This is the table view from the storyboard !!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Refering the table view data source as self for get their own reference
+        tableView.dataSource = self
+        
+        // I don't understand much about this... but is the way for call the readTrips function
+        // I made this function with the dispatcher queue for get priotity to this threat
         TripFunctions.readTrips(completition: { [weak self] in
             self?.tableView.reloadData()
         })
+    }
+    
+    // this functions you have to implement as part of the UITableViewDataSource protocol and the delegate pattern
+    
+    // how many rows you will have ?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.tripModels.count
+    }
+    
+    // What are you show in every row? 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cellId")
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cellId")
+        }
+        cell!.textLabel?.text = Data.tripModels[indexPath.row].title
+        return cell!
     }
 }
