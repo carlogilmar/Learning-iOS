5de256a255d3686a70fc0068e86edf4b9111506f
carlogilmar <carlogilmar12@gmail.com>
2018-08-14 00:37:57 -0500

    Adding controller and storyboard for TripsViewController

---
+++ itineraty/itineraty/Views/TripsViewController/TripsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TripsViewController.swift
+//  itineraty
+//
+//  Created by Carlo Gilmar on 14/08/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import UIKit
+
+class TripsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
