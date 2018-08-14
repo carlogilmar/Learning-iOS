32709aa6dd3077965e0f8e11a3258f072c76ecda
carlogilmar <carlogilmar12@gmail.com>
2018-08-14 00:24:06 -0500

    Adding data.swift for mock models

---
+++ itineraty/itineraty/Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  itineraty
+//
+//  Created by Carlo Gilmar on 14/08/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    
+    static var tripModels = [TripModel]()
+}
