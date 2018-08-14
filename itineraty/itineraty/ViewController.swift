4d1744825f4c9d090f9e7aeace3f5c70b93e1b63
carlogilmar <carlogilmar12@gmail.com>
2018-08-13 23:47:57 -0500

    Adding itineraty project

---
+++ itineraty/itineraty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  itineraty
+//
+//  Created by Carlo Gilmar on 13/08/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
