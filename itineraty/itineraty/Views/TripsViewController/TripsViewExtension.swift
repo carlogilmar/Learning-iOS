ae8c9966704e7263e5d10d3cdec312d586e1083b
carlogilmar <carlogilmar12@gmail.com>
2018-08-27 02:03:17 -0500

    Adding a controller extension

--- itineraty/itineraty/Views/TripsViewController/TripsViewExtension.swift
+++ itineraty/itineraty/Views/TripsViewController/TripsViewExtension.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2018 Carlo Gilmar. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension TripsViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.tripModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TripsTableViewCell
+        cell.setup(tripModel: Data.tripModels[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 160
+    }
+}
