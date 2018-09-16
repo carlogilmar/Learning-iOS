0f47685c8c3e09d7de651858888cf457c4bdff21
carlogilmar <carlogilmar12@gmail.com>
2018-09-15 23:11:18 -0500

    Adding table view and delegate in viewController

--- udemy_exercises/learning/learning/ViewController.swift
+++ udemy_exercises/learning/learning/ViewController.swift
@@ -8,42 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        table.dataSource = self
+        table.delegate = self
     }
     
-    override func loadView() {
-        super.loadView()
-        print("1.- Loading view")
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        print("2.- View will appear")
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "holaaaa"
+        return cell
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
-        print("3.- View did appear")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-        print("4.- View will disappear")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-        print("5.- View did disappear")
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        print("6.- Did receive memory warning!")
-    }
-    
+
 }
 
