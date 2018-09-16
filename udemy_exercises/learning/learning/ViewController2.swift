fdcd9cfdec85755e73ba8b70c84d3647ea04240d
carlogilmar <carlogilmar12@gmail.com>
2018-09-15 20:18:35 -0500

    Adding navigation controller

---
+++ udemy_exercises/learning/learning/ViewController2.swift
@@ -0,0 +1,49 @@
+//
+//  screen2.swift
+//  learning
+//
+//  Created by Carlo Gilmar on 15/09/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func loadView() {
+        super.loadView()
+        print("1.- Loading view")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print("2.- View will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print("3.- View did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        print("4.- View will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        print("5.- View did disappear")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        print("6.- Did receive memory warning!")
+    }
+    
+}
+
